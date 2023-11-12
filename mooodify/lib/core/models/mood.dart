13a4dbd68a847b98420f20a697cbb56eb6b56c75
timsedev <timsedev@gmail.com>
2023-11-12 18:18:30 +0800

    change enums to camelcase and use extension to capitalise the names

--- mooodify/lib/core/models/mood.dart
+++ mooodify/lib/core/models/mood.dart
@@ -1,8 +1,29 @@
 enum Mood {
-  Depressed,
-  Stressed,
-  Neutral,
-  Happy,
-  Excited,
-  None,
+  depressed,
+  stressed,
+  neutral,
+  happy,
+  excited,
+  none,
+}
+
+extension MoodExtension on Mood {
+  String get name {
+    switch (this) {
+      case Mood.depressed:
+        return 'Depressed';
+      case Mood.stressed:
+        return 'Stressed';
+      case Mood.neutral:
+        return 'Neutral';
+      case Mood.happy:
+        return 'Happy';
+      case Mood.excited:
+        return 'Excited';
+      case Mood.none:
+        return 'None';
+      default:
+        return 'None';
+    }
+  }
 }
