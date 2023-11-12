ffa01d726779d20c72ab345d5356e55657a62bf5
timsedev <timsedev@gmail.com>
2023-11-12 17:43:29 +0800

    added moodByDay and its setter & getter in moodservice

--- mooodify/lib/services/mood_service.dart
+++ mooodify/lib/services/mood_service.dart
@@ -1,1 +1,13 @@
-class MoodService {}
+import 'package:mooodify/core/models/mood.dart';
+
+class MoodService {
+  Map<DateTime, Mood> moodByDay = {};
+
+  void addMood(DateTime date, Mood mood) {
+    moodByDay[date] = mood;
+  }
+
+  Mood? getMood(DateTime date) {
+    return moodByDay[date];
+  }
+}
