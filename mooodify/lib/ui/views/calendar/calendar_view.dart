eeda11606478d8462324fbbb157ceffab365f6a0
timsedev <timsedev@gmail.com>
2023-11-13 15:51:30 +0800

    added TableCalendar and basic styling

--- mooodify/lib/ui/views/calendar/calendar_view.dart
+++ mooodify/lib/ui/views/calendar/calendar_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import 'calendar_viewmodel.dart';
 
@@ -13,9 +14,51 @@ class CalendarView extends StackedView<CalendarViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      body: SafeArea(
+        child: TableCalendar(
+          firstDay: DateTime.utc(2020, 1, 1),
+          lastDay: DateTime.utc(2050, 31, 12),
+          focusedDay: DateTime.now(),
+          calendarFormat: CalendarFormat.month,
+          startingDayOfWeek: StartingDayOfWeek.monday,
+          headerStyle: const HeaderStyle(
+            headerMargin: EdgeInsets.symmetric(vertical: 15), // Add this line
+            headerPadding: EdgeInsets.all(10),
+            titleCentered: true,
+            titleTextStyle: TextStyle(
+              fontSize: 20,
+            ),
+            leftChevronIcon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+              size: 15,
+            ),
+            rightChevronIcon: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.black,
+              size: 15,
+            ),
+          ),
+          availableCalendarFormats: const {
+            CalendarFormat.month: 'Month',
+          },
+          calendarBuilders: CalendarBuilders(
+            todayBuilder: (context, day, focusedDay) => Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.black,
+              ),
+              child: Center(
+                child: Text(
+                  day.day.toString(),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
