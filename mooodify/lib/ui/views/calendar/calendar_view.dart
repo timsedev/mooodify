0b63bf8ca339a487ee4092a8a2a18e15271c2db4
timsedev <timsedev@gmail.com>
2023-11-21 21:37:25 +0800

    added padding to calendar view and changed first day

--- mooodify/lib/ui/views/calendar/calendar_view.dart
+++ mooodify/lib/ui/views/calendar/calendar_view.dart
@@ -15,68 +15,71 @@ class CalendarView extends StackedView<CalendarViewModel> {
   ) {
     return Scaffold(
       body: SafeArea(
-        child: TableCalendar(
-          firstDay: DateTime.utc(2020, 1, 1),
-          lastDay: DateTime.utc(2050, 31, 12),
-          focusedDay: DateTime.now(),
-          calendarFormat: CalendarFormat.month,
-          startingDayOfWeek: StartingDayOfWeek.monday,
-          headerStyle: const HeaderStyle(
-            headerMargin: EdgeInsets.symmetric(vertical: 15), // Add this line
-            headerPadding: EdgeInsets.all(10),
-            titleCentered: true,
-            titleTextStyle: TextStyle(
-              fontSize: 20,
-            ),
-            leftChevronIcon: Icon(
-              Icons.arrow_back_ios,
-              color: Colors.black,
-              size: 15,
-            ),
-            rightChevronIcon: Icon(
-              Icons.arrow_forward_ios,
-              color: Colors.black,
-              size: 15,
-            ),
-          ),
-          availableCalendarFormats: const {
-            CalendarFormat.month: 'Month',
-          },
-          calendarBuilders: CalendarBuilders(
-            todayBuilder: (context, day, focusedDay) => Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: TableCalendar(
+            firstDay: DateTime.utc(2023, 1, 1),
+            lastDay: DateTime.utc(2050, 31, 12),
+            focusedDay: DateTime.now(),
+            calendarFormat: CalendarFormat.month,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            headerStyle: const HeaderStyle(
+              headerMargin: EdgeInsets.symmetric(vertical: 15), // Add this line
+              headerPadding: EdgeInsets.all(10),
+              titleCentered: true,
+              titleTextStyle: TextStyle(
+                fontSize: 20,
               ),
-              child: Center(
-                child: Text(
-                  day.day.toString(),
-                  style: TextStyle(
-                    color: Colors.white,
+              leftChevronIcon: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+                size: 15,
+              ),
+              rightChevronIcon: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
+                size: 15,
+              ),
+            ),
+            availableCalendarFormats: const {
+              CalendarFormat.month: 'Month',
+            },
+            calendarBuilders: CalendarBuilders(
+              todayBuilder: (context, day, focusedDay) => Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.black,
+                ),
+                child: Center(
+                  child: Text(
+                    day.day.toString(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            markerBuilder: (context, date, events) {
-              if (events.isNotEmpty) {
-                return Positioned(
-                  bottom: 5,
-                  child: Container(
-                    width: 5,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: _setDotColor(date),
+              markerBuilder: (context, date, events) {
+                if (events.isNotEmpty) {
+                  return Positioned(
+                    bottom: 5,
+                    child: Container(
+                      width: 5,
+                      height: 5,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _setDotColor(date),
+                      ),
                     ),
-                  ),
-                );
-              }
-              return Container();
+                  );
+                }
+                return Container();
+              },
+            ),
+            eventLoader: (day) {
+              return viewModel.getMoodsForDay(day);
             },
           ),
-          eventLoader: (day) {
-            return viewModel.getMoodsForDay(day);
-          },
         ),
       ),
     );
