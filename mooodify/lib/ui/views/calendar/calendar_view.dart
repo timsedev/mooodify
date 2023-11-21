1b215da0fd40b901ab57feb2c47d9dd8cfaed150
timsedev <timsedev@gmail.com>
2023-11-21 21:40:57 +0800

    extract table calendar to a method

--- mooodify/lib/ui/views/calendar/calendar_view.dart
+++ mooodify/lib/ui/views/calendar/calendar_view.dart
@@ -17,69 +17,7 @@ class CalendarView extends StackedView<CalendarViewModel> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(10),
-          child: TableCalendar(
-            firstDay: DateTime.utc(2023, 1, 1),
-            lastDay: DateTime.utc(2050, 31, 12),
-            focusedDay: DateTime.now(),
-            calendarFormat: CalendarFormat.month,
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            headerStyle: const HeaderStyle(
-              headerMargin: EdgeInsets.symmetric(vertical: 15), // Add this line
-              headerPadding: EdgeInsets.all(10),
-              titleCentered: true,
-              titleTextStyle: TextStyle(
-                fontSize: 20,
-              ),
-              leftChevronIcon: Icon(
-                Icons.arrow_back_ios,
-                color: Colors.black,
-                size: 15,
-              ),
-              rightChevronIcon: Icon(
-                Icons.arrow_forward_ios,
-                color: Colors.black,
-                size: 15,
-              ),
-            ),
-            availableCalendarFormats: const {
-              CalendarFormat.month: 'Month',
-            },
-            calendarBuilders: CalendarBuilders(
-              todayBuilder: (context, day, focusedDay) => Container(
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.black,
-                ),
-                child: Center(
-                  child: Text(
-                    day.day.toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              markerBuilder: (context, date, events) {
-                if (events.isNotEmpty) {
-                  return Positioned(
-                    bottom: 5,
-                    child: Container(
-                      width: 5,
-                      height: 5,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: _setDotColor(date),
-                      ),
-                    ),
-                  );
-                }
-                return Container();
-              },
-            ),
-            eventLoader: (day) {
-              return viewModel.getMoodsForDay(day);
-            },
-          ),
+          child: _buildCalendar(context, viewModel),
         ),
       ),
     );
@@ -101,5 +39,71 @@ class CalendarView extends StackedView<CalendarViewModel> {
     }
 
     return Colors.black;
+  }
+
+  Widget _buildCalendar(BuildContext context, CalendarViewModel viewModel) {
+    return TableCalendar(
+      firstDay: DateTime.utc(2023, 1, 1),
+      lastDay: DateTime.utc(2050, 31, 12),
+      focusedDay: DateTime.now(),
+      calendarFormat: CalendarFormat.month,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      headerStyle: const HeaderStyle(
+        headerMargin: EdgeInsets.symmetric(vertical: 15), // Add this line
+        headerPadding: EdgeInsets.all(10),
+        titleCentered: true,
+        titleTextStyle: TextStyle(
+          fontSize: 20,
+        ),
+        leftChevronIcon: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+          size: 15,
+        ),
+        rightChevronIcon: Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.black,
+          size: 15,
+        ),
+      ),
+      availableCalendarFormats: const {
+        CalendarFormat.month: 'Month',
+      },
+      calendarBuilders: CalendarBuilders(
+        todayBuilder: (context, day, focusedDay) => Container(
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.black,
+          ),
+          child: Center(
+            child: Text(
+              day.day.toString(),
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        markerBuilder: (context, date, events) {
+          if (events.isNotEmpty) {
+            return Positioned(
+              bottom: 5,
+              child: Container(
+                width: 5,
+                height: 5,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _setDotColor(date),
+                ),
+              ),
+            );
+          }
+          return Container();
+        },
+      ),
+      eventLoader: (day) {
+        return viewModel.getMoodsForDay(day);
+      },
+    );
   }
 }
