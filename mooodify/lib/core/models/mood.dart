85f2e3f45d9a036e64b1bbcc9e4d06033369736a
timsedev <timsedev@gmail.com>
2023-11-23 16:55:06 +0800

    revamped Mood from enum to class

--- mooodify/lib/core/models/mood.dart
+++ mooodify/lib/core/models/mood.dart
@@ -1,37 +1,49 @@
-enum Mood {
-  depressed,
-  stressed,
+class Mood {
+  MoodType moodType;
+  double moodValue;
+
+  Mood({
+    required this.moodType,
+    required this.moodValue,
+  });
+
+  static List<Mood> allMoods() {
+    return [
+      Mood(moodType: MoodType.terrible, moodValue: -2),
+      Mood(moodType: MoodType.bad, moodValue: -1),
+      Mood(moodType: MoodType.neutral, moodValue: 0),
+      Mood(moodType: MoodType.happy, moodValue: 1),
+      Mood(moodType: MoodType.excellent, moodValue: 2),
+    ];
+  }
+}
+
+enum MoodType {
+  terrible,
+  bad,
   neutral,
   happy,
-  excited,
+  excellent,
   none,
 }
 
-extension MoodExtension on Mood {
+extension MoodExtension on MoodType {
   String get moodName {
     switch (this) {
-      case Mood.depressed:
-        return 'Depressed';
-      case Mood.stressed:
-        return 'Stressed';
-      case Mood.neutral:
+      case MoodType.terrible:
+        return 'Terrible';
+      case MoodType.bad:
+        return 'Bad';
+      case MoodType.neutral:
         return 'Neutral';
-      case Mood.happy:
+      case MoodType.happy:
         return 'Happy';
-      case Mood.excited:
-        return 'Excited';
-      case Mood.none:
+      case MoodType.excellent:
+        return 'Excellent';
+      case MoodType.none:
         return 'None';
       default:
         return 'None';
     }
   }
 }
-
-final List<Mood> availableMoods = [
-  Mood.depressed,
-  Mood.stressed,
-  Mood.neutral,
-  Mood.happy,
-  Mood.excited,
-];
