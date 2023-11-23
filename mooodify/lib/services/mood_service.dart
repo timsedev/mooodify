02090ba4caeeebe9307ff31859806b3c158e15a9
timsedev <timsedev@gmail.com>
2023-11-23 16:56:25 +0800

    revamped new Mood in MoodService

--- mooodify/lib/services/mood_service.dart
+++ mooodify/lib/services/mood_service.dart
@@ -3,16 +3,17 @@ import 'dart:developer';
 import 'package:mooodify/core/models/mood.dart';
 
 class MoodService {
-  Map<DateTime, Mood> moodByDay = {};
+  Map<DateTime, Mood?> moodByDay = {};
+  List<Mood> allMoods = Mood.allMoods();
 
   void addMood(DateTime datetime, Mood mood) {
     final date = DateTime(datetime.year, datetime.month, datetime.day);
     moodByDay[date] = mood;
   }
 
-  Mood getMood(DateTime datetime) {
+  Mood? getMood(DateTime datetime) {
     final date = DateTime(datetime.year, datetime.month, datetime.day);
-    return moodByDay[date] ?? Mood.none;
+    return moodByDay[date];
   }
 
   /// For debugging purposes
