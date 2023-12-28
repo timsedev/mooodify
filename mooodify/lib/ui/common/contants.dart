9cb61aaabbd08bf009e732ff390a15b0cc5e9bbc
timsedev <timsedev@gmail.com>
2023-12-28 16:45:10 +0800

    Add mood orbit color gradients into constants file

---
+++ mooodify/lib/ui/common/contants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// All colors for the mood orbit
+final terribleGradient = [Colors.red[900]!, Colors.red];
+final badGradient = [Colors.orange[900]!, Colors.orange];
+final neutralGradient = [Colors.grey[600]!, Colors.grey[400]!];
+final happyGradient = [Colors.lightGreen, Colors.yellow];
+final excellentGradient = [Colors.green[900]!, Colors.lightGreenAccent[700]!];
