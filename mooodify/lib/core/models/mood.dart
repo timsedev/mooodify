bb7d15ea1c2bfdd8b3940669cd2314b347f72e87
Tim <timsedev@gmail.com>
2024-03-11 15:05:12 +0800

    Updated mood model class

--- mooodify/lib/core/models/mood.dart
+++ mooodify/lib/core/models/mood.dart
@@ -1,11 +1,15 @@
 import 'dart:ui';
 
 class Mood {
-  String type;
-  double value;
+  final String id;
+  final String type;
+  final double value;
+  final DateTime updatedAt;
 
   Mood({
+    required this.id,
     required this.type,
     required this.value,
+    required this.updatedAt,
   });
 }
